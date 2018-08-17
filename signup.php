fac857eecdae57446e4981e62197ef37216b1117
Davematics <42449119+Davematics@users.noreply.github.com>
2018-08-16 23:35:20 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,11 +1,31 @@
 <?php include("config.php");
+
+$Id=$_POST['Id'];
+$email=$_POST['email'];
+$name=$_POST['name'];
+$password=$_POST['password'];
 if(array_key_exists("submit",$_POST)){
-	move_uploaded_file($_FILES["file"]["tmp_name"], "photos/" . $_FILES["file"]["name"]);
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/jpg")
+|| ($_FILES["file"]["type"] == "image/pjpeg")
+|| ($_FILES["file"]["type"] == "image/x-png")
+|| ($_FILES["file"]["type"] == "image/png"))
+
+)
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+  
+      move_uploaded_file($_FILES["file"]["tmp_name"], "photos/" . $_FILES["file"]["name"]);
      // echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
 	  $imageurl = "photos/" . $_FILES["file"]["name"];
-//$result=mysqli_query($link,"select * from ads");
-extract($_POST);
-$query=mysqli_query($link,"insert into sqtdb_test(Id ,name ,email ,password ,picture) values('$Id','$name','$email','$password','$imageurl')")or die("cannot add new ads".mysqli_error($link));
+$result=mysqli_query($link,"select * from sqtdb_test");
+$query=mysqli_query($link,"insert into sqtdb_test(Id,name,email,password,picture) values('$Id','$name','$email','$password','$imageurl')");
 if($query==1){
 ?>
 <script type="text/javascript">
@@ -14,182 +34,83 @@ alert("Registration was sucessfully");
 <?php
 }else{?>
     <script type="text/javascript">
-alert("process was not successfull ");
+alert("process was not successfull choose a Different username ");
 </script>
 <?php
 }
-
+}
+}
 }
 ?>
-<script>
-function validateForm() {
-    var x = document.forms["form"]["email"].value;
-    var atpos = x.indexOf("@");
-    var dotpos = x.lastIndexOf(".");
-    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=x.length) {
-        alert("Invalid Email Address");
-        return false;
-    }
-}
-</script>
-<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
-
- <style>
-    #form label{float:left; width:140px;}
-    #error_msg{color:red; font-weight:bold;}
- </style>
-
- <script>
-    $(document).ready(function(){
-        var $submitBtn = $("#form input[type='submit']");
-        var $passwordBox = $("#password");
-        var $confirmBox = $("#confirm_password");
-        var $errorMsg =  $('<span id="error_msg">Passwords do not match.</span>');
-
-        // This is incase the user hits refresh - some browsers will maintain the disabled state of the button.
-        $submitBtn.removeAttr("disabled");
-
-        function checkMatchingPasswords(){
-            if($confirmBox.val() != "" && $passwordBox.val != ""){
-                if( $confirmBox.val() != $passwordBox.val() ){
-                    $submitBtn.attr("disabled", "disabled");
-                    $errorMsg.insertAfter($confirmBox);
-                }
-            }
-        }
-
-        function resetPasswordError(){
-            $submitBtn.removeAttr("disabled");
-            var $errorCont = $("#error_msg");
-            if($errorCont.length > 0){
-                $errorCont.remove();
-            }  
-        } $("#confirm_password, #txtpass")
-             .on("keydown", function(e){
-                /* only check when the tab or enter keys are pressed
-                 * to prevent the method from being called needlessly  */
-                if(e.keyCode == 13 || e.keyCode == 9) {
-                    checkMatchingPasswords();
-                }
-             })
-             .on("blur", function(){                    
-                // also check when the element looses focus (clicks somewhere else)
-                checkMatchingPasswords();
-            })
-            .on("focus", function(){
-                // reset the error message when they go to make a change
-                resetPasswordError();
-            })
-
-    });
-  </script>
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<link href="css.css" rel="stylesheet" type="text/css">
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
-
 <!DOCTYPE html>
-<html lang="en">
-    <head> 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-
-		<!-- Website CSS style -->
-		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-
-		<!-- Website Font style -->
-	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-		
-		<!-- Google Fonts -->
-		<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
-		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
-
-		<title>Sign Up</title>
-	</head>
+<html>
+<head>
+<title>Sign Up </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta name="keywords" content="Sowing a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/smoothbox.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href="//fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
 <body>
-		<div class="container">
-			<div class="row main">
-				<div class="panel-heading">
-	               <div class="panel-title text-center">
-	               		<h1 class="title">Sign Up</h1>
-	               		<hr />
-               	  </div>
-	            </div> 
-				<div class="main-login main-center">
-					<form class="form-horizontal" method="post" name="form" action="" onsubmit="return validateForm();">
-						<div class="form-group">
-							<label for="name" class="cols-sm-2 control-label">Username</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="Id" id="Id"  placeholder="Choose a Username"/>
-								</div>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="name" class="cols-sm-2 control-label">Your Name</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-user fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="name" id="name"  placeholder="Enter your Name"/>
-								</div>
-							</div>
-						</div>
+<!-- Banner-->
+<!-- navigation -->
+</br></br>
+<!-- /Banner-->
 
-						<div class="form-group">
-							<label for="email" class="cols-sm-2 control-label">Your Email</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
-									<input type="text" class="form-control" name="email" id="email"  placeholder="Enter your Email"/>
-								</div>
-							</div>
-						</div>
 
-						
+<!-- News-->
+</br></br>
+<!--/News-->
+<div align="center"><h1><a href="login.php">Click To Login</a></h1> </div>
+<div align="center"><h1>Sign Up</h1> </div>
 
-						<div class="form-group">
-							<label for="password" class="cols-sm-2 control-label">Password</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-									<input type="password" class="form-control" name="password" id="password"  placeholder="Enter your Password"/>
-								</div>
-							</div>
-						</div>
+</div>
 
-						<div class="form-group">
-							<label for="confirm" class="cols-sm-2 control-label">Confirm Password</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
-									<input type="password" class="form-control" name="confirm_password" id="confirm_password"  placeholder="Confirm your Password"/>
-								</div>
-							</div>
-						</div>
+ <div class="container">
+  <form action="" method="post"  enctype="multipart/form-data" name="form1" >
+
 <div class="form-group">
-							<label for="file" class="cols-sm-2 control-label">Photo</label>
-							<div class="cols-sm-10">
-								<div class="input-group">
-									<span class="input-group-addon"><i class="fa fa-users fa" aria-hidden="true"></i></span>
-									<input name="file" type="file">
-								</div>
-							</div>
-						</div>
-						<div class="form-group ">
-							
-                            <input type="submit" name="submit" class="btn btn-primary btn-lg btn-block login-button" id="button" value="Submit" />
-						</div>
-						<div class="login-register">
-				            <a href="login.php">Login</a>
-			          </div>
-					</form>
-				</div>
-			</div>
-		</div>
+<label for="lastName" class="col-sm-2 control-label">	Username:</label>
+<input type="text" name="Id" class="form-control" id="ld" placeholder="Choose a username">
+</div>
+ 
 
-		<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-</body>
+<div class="form-group">
+<label for="dob" class="col-sm-2 control-label">Your Name:</label>
+<input type="text" name="name" class="form-control" id="name" placeholder=" Enter Your Name">
+
+</div>
+<div class="form-group">
+<label for="dob" class="col-sm-2 control-label">Email:</label>
+<input type="email" name="email" class="form-control" id="name" placeholder=" Enter Your Email">
+
+</div>
+
+ 
+<div class="form-group">
+<label for="passwd" class="col-sm-2 control-label">Password:</label>
+<input type="password" name="password" class="form-control" id="passwd" placeholder="Password">
+</div>
+ 
+<div class="form-group">
+<label for="passwd" class="col-sm-2 control-label">PHOTO: REQUIRED</label>
+<input name="file" type="file">
+</div>
+
+<div class="form-group">
+
+<input type="submit" name="submit" class="btn btn-primary" id="button" value="Submit" />
+
+</div>
+ 
+</form>
+ </div>
+ </body>
 </html>
